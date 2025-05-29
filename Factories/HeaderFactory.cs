4697b77f1731955d703fcccc8b20e38fece5f657
MichaelSuthirta <michaelsuthirta@gmail.com>
2025-05-29 09:49:44 +0700

    Added User, Transaction Details, Header and Jewels factory

---
+++ Factories/HeaderFactory.cs
@@ -0,0 +1,22 @@
+﻿using PSDLabProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PSDLabProject.Factories
+{
+    public class HeaderFactory
+    {
+        public static TransactionHeader createHeader(int userID, DateTime date, string paymentMethod, string status)
+        {
+            return new TransactionHeader()
+            {
+                UserID = userID,
+                TransactionDate = date,
+                PaymentMethod = paymentMethod,
+                TransactionStatus = status
+            };
+        }
+    }
+}
---
+++ Factories/UserFactory.cs
@@ -0,0 +1,24 @@
+﻿using PSDLabProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PSDLabProject.Factories
+{
+    public class UserFactory
+    {
+        public static MsUser createUser(string name, string email, string password, DateTime dob, string gender, string role)
+        {
+            return new MsUser()
+            {
+                UserName = name,
+                UserEmail = email,
+                UserPassword = password,
+                UserDOB = dob,
+                UserGender = gender,
+                UserRole = role
+            };
+        }
+    }
+}
