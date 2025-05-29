2a17781a1b0f739b99a7d1516d6ad8911af4daa6
MichaelSuthirta <michaelsuthirta@gmail.com>
2025-05-29 11:34:32 +0700

    Added User and Jewel Repository

---
+++ Repositories/JewelRepository.cs
@@ -0,0 +1,31 @@
+﻿using PSDLabProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PSDLabProject.Repositories
+{
+    public class JewelRepository
+    {
+        private static DatabaseProjectEntities db = new DatabaseProjectEntities();
+
+        public static MsJewel findJewelByID(int jewelID)
+        {
+            return db.MsJewels.Where(x => x.JewelID == jewelID).FirstOrDefault();
+        }
+        public static string addJewel(MsJewel jewel)
+        {
+            db.MsJewels.Add(jewel);
+            db.SaveChanges();
+            return "Jewel added successfully.";
+        }
+
+        public static string deleteJewel(MsJewel jewel)
+        {
+            db.MsJewels.Remove(jewel);
+            db.SaveChanges();
+            return "Jewel removed successfully.";
+        }
+    }
+}
