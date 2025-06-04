ae47651a78abe1acb5bcbade433592448b8e418a
Muhammad Rafi Isnaen <muhammadrafiisnaen@gmail.com>
2025-06-04 08:51:04 +0700

    Changing JewelViewer.aspx
    Change from standard WebForm to WebForm with master page

--- Views/JewelPages/JewelViewer.aspx.cs
+++ Views/JewelPages/JewelViewer.aspx.cs
@@ -9,7 +9,7 @@ using System.Web.UI.WebControls;
 
 namespace PSDLabProject.Views.JewelPages
 {
-    public partial class ViewJewels : System.Web.UI.Page
+    public partial class JewelViewer : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
--- Views/JewelPages/JewelViewer.aspx.designer.cs
+++ Views/JewelPages/JewelViewer.aspx.designer.cs
@@ -11,17 +11,8 @@ namespace PSDLabProject.Views.JewelPages
 {
 
 
-    public partial class ViewJewels
+    public partial class JewelViewer
     {
-
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
         /// UserJewelGridview control.
